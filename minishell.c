0b1049ef4c3585a23472e7482cc765f6743cad7c
Carmen Gonzalez Hidalgo <carmegon@c2r3s4.42malaga.com>
2025-05-14 17:53:18 +0200

    creando ft_putstr

--- minishell.c
+++ minishell.c
@@ -1,3 +1,17 @@
+#include <string.h>
+#include <unistd.h>
+
+char	ft_putstr(char *s)
+{
+	int	i;
+	while (s[i] != '\0')
+	{
+		write(1, &s[i], 1);
+	}
+	return(s);
+}
+
+
 int	main(void)
 {
 	prinf("Hola mundo %");
